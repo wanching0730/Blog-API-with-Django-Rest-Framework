80f3baee82405ec8434d6356685c85d3ac81a36f
wanching_97 <wanching_97@hotmail.com>
2018-05-17 10:08:21 +0800

    add angular urls into django

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -18,16 +18,12 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-
-
+from django.views.generic.base import TemplateView
 from rest_framework_jwt.views import obtain_jwt_token
-
-from accounts.views import (login_view, register_view, logout_view)
 
 urlpatterns = [
     
     url(r'^admin/', admin.site.urls),
-
     url(r'^api/auth/token/', obtain_jwt_token),
     url(r'^api/users/', include("accounts.api.urls", namespace='users-api')),
     url(r'^api/comments/', include("comments.api.urls", namespace='comments-api')),
@@ -38,3 +34,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# due to Single Page issue in Angular, the angular urls have to be separated from Django urls
+urlpatterns += [
+    url(r'', TemplateView.as_view(template_name='ang/home.html'))
+]
